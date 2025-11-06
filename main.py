5c4a1e889fd383a0fdc188d0f797077f528f4d7a
kirvitchenko <vitchenkokir@gmail.com>
2025-11-06 17:23:42 +0300

    Final version

--- main.py
+++ main.py
@@ -5,22 +5,22 @@ from tabulate import tabulate
 
 
 def get_complete_devices_list(files):
-    """
-    Получаем список словарей с данными устройств из сформированный из данных разных файлов
-    """
+    """Получаем список словарей с данными устройств из сформированный из данных разных файлов"""
+
     full_list = []
     for file in files:
-        with open(file, encoding="utf-8") as f:
-            reader = csv.DictReader(f)
-            for device_dict in reader:
-                full_list.append(device_dict)
+        try:
+            with open(file, encoding="utf-8") as f:
+                reader = csv.DictReader(f)
+                for device_dict in reader:
+                    full_list.append(device_dict)
+        except FileNotFoundError:
+            print(f"Файл {file} не найден. Пропускаем")
     return full_list
 
 
 def get_all_values_by_field(field, devices_list):
-    """
-    Получаем словарь со списками значений небходимого поля для каждого бренда
-    """
+    """Получаем словарь со списками значений небходимого поля для каждого бренда"""
     values_dict = {}
     for device in devices_list:
         if device["brand"] in values_dict:
@@ -32,25 +32,19 @@ def get_all_values_by_field(field, devices_list):
 
 def get_report(report_type, values_dict):
     """
-    Получаем отсортированный список и пронумерованный список кортежей c данными по необходимому типу отчета
+    Получаем отсортированный список кортежей c данными по необходимому типу отчета
     В параметре report_type указывается тип отчета и за счет ветвления выполнятся необходимая логика
     для формирования отчета
-    :param report_type:
-    :param values_dict:
-    :return:
     """
     report_dict = {}
     if report_type == "average":
         for device_field in values_dict:
             report_dict[device_field] = round(mean(values_dict[device_field]), 2)
     sorted_report = sorted(report_dict.items(), key=lambda x: x[1], reverse=True)
-    table_data = [
-        (i + 1, brand, rating) for i, (brand, rating) in enumerate(sorted_report)
-    ]
-    return table_data
+    return sorted_report
 
 
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser(description="Скрипт для анализа csv-файла")
 
     parser.add_argument(
@@ -74,5 +68,14 @@ if __name__ == "__main__":
     devices_list = get_complete_devices_list(args.files)
     report_dict = get_all_values_by_field(field, devices_list)
     result = get_report(type_of_report, report_dict)
+    table_data = [(i + 1, brand, rating) for i, (brand, rating) in enumerate(result)]
 
-    print(tabulate(result, headers=["Бренд", "Средний рейтинг"], tablefmt="grid"))
+    print(
+        tabulate(
+            table_data, headers=["№", "Бренд", "Средний рейтинг"], tablefmt="fancy_grid"
+        )
+    )
+
+
+if __name__ == "__main__":
+    main()
