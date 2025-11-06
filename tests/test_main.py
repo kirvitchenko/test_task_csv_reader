163b674448aefeab3021a348296675639d32cd4b
kirvitchenko <vitchenkokir@gmail.com>
2025-11-06 17:24:20 +0300

    Final version

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,6 @@
-from main import get_report, get_all_values_by_field, get_complete_devices_list
+from unittest.mock import patch
+
+from main import get_report, get_all_values_by_field, get_complete_devices_list, main
 
 test_report_dict = {
     "apple": [4.8, 4.5, 4.6, 4.3, 4.7, 4.9, 4.4, 4.6, 4.7, 4.5],
@@ -54,3 +56,48 @@ def test_get_all_values_by_field_price():
         "google": [699.0],
     }
     assert result == expected
+
+
+def test_get_complete_devices_list():
+    result = get_complete_devices_list(["tests/test_csv.csv"])
+    expected = [
+        {"name": "iphone 15 pro", "brand": "apple", "price": "999", "rating": "4.9"},
+        {
+            "name": "galaxy s23 ultra",
+            "brand": "samsung",
+            "price": "1199",
+            "rating": "4.8",
+        },
+        {"name": "redmi note 12", "brand": "xiaomi", "price": "199", "rating": "4.6"},
+        {"name": "iphone 14", "brand": "apple", "price": "799", "rating": "4.7"},
+        {"name": "galaxy a54", "brand": "samsung", "price": "349", "rating": "4.2"},
+    ]
+    assert result == expected
+
+
+def test_get_complete_devices_list_false():
+    result = get_complete_devices_list(["tests/xxx.csv"])
+    expected = []
+    assert result == expected
+
+
+def test_scripts_run_succesfully():
+    with patch(
+        "sys.argv",
+        ["main.py", "--files", "tests/test_csv.csv", "--report", "average-rating"],
+    ):
+        with patch("sys.stdout") as mock_stdout:
+            main()
+
+            assert mock_stdout.write.called
+
+            all_calls = mock_stdout.write.call_args_list
+            print(all_calls)
+
+            full_output = ""
+            for call in all_calls:
+                if call[0]:
+                    full_output += call[0][0]
+
+    assert "Бренд" in full_output, "Должна быть выведена таблица с заголовком"
+    assert "Средний рейтинг" in full_output, "Должна быть выведена таблица с рейтингами"
